520b9f4087491ba25eff15b29cb7f42172e9c834
Andres Briñez <brinezlopez08@gmail.com>
2024-07-05 22:20:15 -0500

    Se añaden las rutas al proyecto

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Register from './Pages/Register/Register';
+import RoutesHandler from './Router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <Register/>
+  <RoutesHandler/>
   </React.StrictMode>
 );
 
