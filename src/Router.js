c887cec6cc4d4778f77a12ccf6345547be02966b
Andres Briñez <brinezlopez08@gmail.com>
2024-07-07 00:50:34 -0500

    Se agrega la ruta de home

--- src/Router.js
+++ src/Router.js
@@ -2,12 +2,13 @@ import { BrowserRouter as Router, Switch, Route,Routes } from 'react-router-dom'
 import Register from "./Pages/Register/Register"
 import Login from "./Pages/Login";
 import Page404 from './Pages/404';
+import Home from './Pages/Home';
 
 const RoutesHandler = () => {
   return (
     <Router>
       <Routes>
-        {/* <Route path="/" exact element={<Register />} /> */}
+        <Route path="/" exact element={<Home />} />
         <Route path="/register" exact element={<Register />} />
         <Route path="/login" element={<Login />} />
         {/* <Route path="/product:id" element={<Product />} /> */}
