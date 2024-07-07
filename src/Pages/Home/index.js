494688a8df1635a2fa30cfd081789a1234a1674e
Andres Briñez <brinezlopez08@gmail.com>
2024-07-07 01:38:21 -0500

    Se crea parcialmente co nel header la página del home

---
+++ src/Pages/Home/index.js
@@ -0,0 +1,13 @@
+import HeaderHome from "../../Components/Header/Home";
+import "./style.css"
+
+function Home() {
+  return <>
+    <HeaderHome/>
+    <div class="banner">
+      {/* <img src="img/header/home/banner.png" alt="Banner/" /> */}
+    </div>
+  </>
+}
+
+export default Home;
