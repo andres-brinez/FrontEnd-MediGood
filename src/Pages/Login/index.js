b812ed719badafde3efd39d11e2f0c0177e27888
Andres Briñez <brinezlopez08@gmail.com>
2024-07-07 00:44:48 -0500

    Se crea lo necesario para la página de login

---
+++ src/Pages/Login/index.js
@@ -0,0 +1,57 @@
+import { NavLink } from "react-router-dom";
+import Footer from "../../Components/Footer";
+import FormLogin from "../../Components/Forms/Login";
+import HeaderAuth from "../../Components/Header/Auth/Header";
+import "./style.css"
+import Button from "../../Components/Forms/button";
+
+const Login =()=>{
+  return <>
+  <HeaderAuth text="Inicio seguro"/>
+  <main>
+        {/* <!-- Primer contenedor con el saludo de bienvenida --> */}
+        <div className="container">
+            <h3>¿Ya tienes cuenta?</h3>
+            <p><strong>¡Bienvenido de nuevo!</strong> Estamos emocionados de verte otra vez.<br/>Por favor, inicia sesión
+                para continuar.</p>
+            <div class="back-to-shop">
+                <a href="ruta/a/la/tienda">
+                    <span>&lt;</span>
+                    <p>Volver a la tienda</p>
+                </a>
+            </div>
+        </div>
+
+        {/* <!-- Segundo contenedor con el formulario de inicio de sesión --> */}
+        <div class="login-container">
+            <h3>Iniciar sesión</h3>
+            <div class="social-login">
+                <button id="button-facebook" type="button">
+                    <a href=""> <img src="img/socialMedia/facebook.svg" alt="Facebook"/>
+                        Facebook</a>
+                </button>
+                <button id="button-google" type="button">
+                    <a href=""><img src="img/socialMedia/google.svg" alt="Google"/>
+                        Google</a>
+                </button>
+            </div>
+            <div class="separator">
+                <div class="line"></div>
+                <p>O</p>
+                <div class="line"></div>
+            </div>
+            <FormLogin/>
+            <div class="separator">
+                <div class="line"></div>
+                <p>¿Eres un nuevo cliente?</p>
+                <div class="line"></div>
+            </div>
+            <Button ruta={"register"}  type="button" text="Crear cuenta"/>
+
+        </div>
+    </main>
+    <Footer/>
+  </>
+}
+
+export default Login;
