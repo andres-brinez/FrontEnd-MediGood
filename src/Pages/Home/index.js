c7f7e69b374ce9109d47dd5cff9bd50a68ac30bc
Andres Briñez <brinezlopez08@gmail.com>
2024-07-07 22:43:29 -0500

    Se agrega el componenente de las categorias

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,3 +1,4 @@
+import Categories from "../../Components/Categories";
 import HeaderHome from "../../Components/Header/Home";
 import "./style.css"
 
@@ -7,6 +8,9 @@ function Home() {
     <div class="banner">
       <img src="img/home/banner.png" alt="Banner/" />
     </div>
+    <main class="containerMain">
+   <Categories/>
+    </main>
   </>
 }
 
