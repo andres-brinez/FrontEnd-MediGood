758d2ddbf58c9442f5db8ea537e1d273507ea986
Andres Briñez <brinezlopez08@gmail.com>
2024-07-10 10:46:56 -0500

    Se agrega las marcas

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -6,7 +6,7 @@ import SliderProducts from "../../Components/SliderProducts";
 
 
 function Home() {
-  
+
   return <>
     <HeaderHome />
     <div class="banner">
@@ -17,16 +17,70 @@ function Home() {
       <section id="recommended">
         <h2>Recomendados para ti</h2>
         <div class="products-container">
-         <SliderProducts products={products}></SliderProducts>
+          <SliderProducts products={products}></SliderProducts>
         </div>
 
+      </section>
+      <section id="brands">
+        <h2>Nuestras marcas más buscadas</h2>
+        <div class="brands-container">
+          <div class="brand">
+            <img src="img/home/marcas/Rectangle 73.png" alt="Marca"/>
+          </div>
+          <div class="brand">
+            <img src="img/home/marcas/rectangle-74.png" alt="Marca"/>
+          </div>
+          <div class="brand">
+            <img src="img/home/marcas/Rectangle 76.png" alt="Marca"/>
+          </div>
+          <div class="brand">
+            <img src="img/home/marcas/Rectangle 77.png" alt="Marca"/>
+          </div>
+          <div class="brand">
+            <img src="img/home/marcas/Rectangle 78.png" alt="Marca"/>
+          </div>
+          <div class="brand">
+            <img src="img/home/marcas/Rectangle 79.png" alt="Marca"/>
+          </div>
+          <div class="brand">
+            <img src="img/home/marcas/Rectangle 80.png" alt="Marca"/>
+          </div>
+          <div class="brand">
+            <img src="img/home/marcas/Rectangle 81.png" alt="Marca"/>
+          </div>
+
+          <div class="brand">
+            <img src="img/home/marcas/Rectangle 82.png" alt="Marca"/>
+          </div>
+          <div class="brand">
+            <img src="img/home/marcas/Rectangle 83.png" alt="Marca"/>
+          </div>
+          <div class="brand">
+            <img src="img/home/marcas/Rectangle 84.png" alt="Marca"/>
+          </div>
+          <div class="brand">
+            <img src="img/home/marcas/Rectangle 85.png" alt="Marca"/>
+          </div>
+          <div class="brand">
+            <img src="img/home/marcas/Rectangle 86.png" alt="Marca"/>
+          </div>
+          <div class="brand">
+            <img src="img/home/marcas/Rectangle 87.png" alt="Marca"/>
+          </div>
+          <div class="brand">
+            <img src="img/home/marcas/Rectangle 88.png" alt="Marca"/>
+          </div>
+          <div class="brand">
+            <img src="img/home/marcas/Rectangle 89.png" alt="Marca"/>
+          </div>
+
+        </div>
       </section>
       <section id="novedad">
         <h2>Novedades</h2>
         <div class="products-container">
-        <SliderProducts products={products}></SliderProducts>
+          <SliderProducts products={products}></SliderProducts>
         </div>
-
       </section>
     </main>
   </>
