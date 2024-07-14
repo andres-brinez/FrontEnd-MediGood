9e8bd377377465b1740d583c703fc6738a4dbd7a
Andres Briñez <brinezlopez08@gmail.com>
2024-07-13 23:26:15 -0500

    Se actualiza el componente

--- src/Components/ProductsSearched/index.js
+++ src/Components/ProductsSearched/index.js
@@ -1,24 +1,48 @@
 import ProductPreview from "../ProductPreview";
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef,useState } from 'react';
+import { useParams } from 'react-router-dom';
+import searchProductByName from "../../services/searchProduct";
+
 
 import "./style.css"
-function ProductsSearched({ searchValue, products }) {
+function ProductsSearched() {
 
   const productsSearchedRef = useRef(null);
+
+  const { searchTerm } = useParams();
+  const [products, setProducts] = useState([])
 
   useEffect(() => {
     // Desplazar la página hacia la sección "products-searched" después de renderizar
     // Se hace de esta manera para asegurar que se haga scroll cuando el componente se haya renderizado
     productsSearchedRef.current.scrollIntoView({ behavior: 'smooth' });
   }, []);
+
+  useEffect(() => {
+
+    if (searchTerm != null) {
+      setProducts(searchProductByName(searchTerm));
+      console.log(products);
+
+      const productsSearchedSection = document.getElementById('products-searched');
+
+      // Se hace un scroll automatico si el componente ya se renderizó, esto es util para cuando se vuelve a hacer una busqueda
+      if (productsSearchedSection != null) {
+        productsSearchedSection.scrollIntoView({ behavior: 'smooth' });
+      }
+
+    }
+
+  }, [searchTerm])
   return <>
+
     <section id="products-searched" ref={productsSearchedRef}>
       <div className="search-results-section">
         {products.length === 0 ?
           <p className="no-results">No hay resultados que coincidan con:</p>:
           <p className="search-term-intro">Productos que coinciden con:</p>
         }
-        <h2 className="search-term">{searchValue.toString()}</h2>
+        <h2 className="search-term">{searchTerm.toString()}</h2>
         <hr className="divider" />
       </div>
       {/* TODO: Se puede agregar el filtro y la cantidad de resultados */}
