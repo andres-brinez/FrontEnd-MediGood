4faaf508de21a8876de36090758f78424ecfef49
Andres Briñez <brinezlopez08@gmail.com>
2024-07-13 23:25:46 -0500

    Se agrega el id al producto  y se permite el redirecionamiento

--- src/Components/ProductPreview/index.js
+++ src/Components/ProductPreview/index.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "./style.css"
 
 const ProductPreview = ({id, urlImg, name, price}) => {
@@ -8,15 +9,16 @@ const ProductPreview = ({id, urlImg, name, price}) => {
   }
   return (
     <div className="product" key={id}>
-      <a href={"ruta/a/la/pagina/del/detalle/del/producto" + id}>
+      <Link to={"/product/"+id}>
         <img src={urlImg} alt={"Producto " + name} className="img-product" />
         <h3 className="name-product">{name}</h3>
         <p> $<span className="precio">{price}</span></p>
-      </a>
+      </Link>
       <div className="cart-button">
-        <img src="img/home/icons-8-shopping-cart.svg" alt="Carrito" />
+        <img src="../img/home/icons-8-shopping-cart.svg" alt="Carrito" />
         Añadir
       </div>
+
     </div>
   );
 };
