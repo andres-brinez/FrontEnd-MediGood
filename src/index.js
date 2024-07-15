ab1b38130f5d586cccddcacfd8386d0a03a9fab9
Andres Briñez <brinezlopez08@gmail.com>
2024-07-15 13:54:40 -0500

    Se acualiza la   importación del globalContext

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import RoutesHandler from './Router';
-import { GlobalContextProvider } from './Context/GlobalContext';
+// import { GlobalContextProvider } from './Context/GlobalContext';
+import GlobalContextProvider from './Context/GlobalContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
