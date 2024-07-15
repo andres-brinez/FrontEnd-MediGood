87bd7e832fcaf2ecc3bc7e83f71ba5b9b72463ee
Andres Briñez <brinezlopez08@gmail.com>
2024-07-15 14:10:42 -0500

    Se crea la funcionalidad de guardar un item en el localStorge y abrir el carrito de compras

--- src/Components/ProductPreview/index.js
+++ src/Components/ProductPreview/index.js
@@ -1,7 +1,31 @@
 import { Link } from "react-router-dom";
 import "./style.css"
 
+import { useContext } from "react";
+// import GlobalContextProvider, { GlobalContext } from '../context/GlobalContext';
+import { useShoppingCart } from "../../hooks/useShoppingCart";
+
+import { GlobalContext } from '../../Context/GlobalContext';
+
 const ProductPreview = ({id, urlImg, name, price}) => {
+
+  // se obtiene el contexto global
+  const {setShowShoppingCart } = useContext(GlobalContext)
+
+  const { saveItemCart } = useShoppingCart();
+  
+  function addCart(){
+    let product = {
+      id: id,
+      image: urlImg,
+      name: name,
+      price: price,
+      quantity: 1
+  };
+  saveItemCart(product);
+  setShowShoppingCart(true);
+
+  }
 
   // Para formatear el numero
   if(price!==undefined){
@@ -14,7 +38,7 @@ const ProductPreview = ({id, urlImg, name, price}) => {
         <h3 className="name-product">{name}</h3>
         <p> $<span className="precio">{price}</span></p>
       </Link>
-      <div className="cart-button">
+      <div className="cart-button" onClick={addCart}>
         <img src="../img/home/icons-8-shopping-cart.svg" alt="Carrito" />
         Añadir
       </div>
