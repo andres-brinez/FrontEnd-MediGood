86c6d14a8001ef32a8a244c51ab8c6cebf41ee99
Andres Briñez <brinezlopez08@gmail.com>
2024-07-15 18:08:28 -0500

    Se verifica que la key de cada items sea unica

--- src/Components/ShoppingCart/index.js
+++ src/Components/ShoppingCart/index.js
@@ -1,24 +1,42 @@
-import { useContext, useState,useEffect } from "react";
+import { useContext, useState, useEffect } from "react";
 import "./style.css"
 import { GlobalContext } from "../../Context/GlobalContext";
 import { useShoppingCart } from "../../hooks/useShoppingCart";
 import CartItem from "../CartItem";
 
 const ShoppingCart = () => {
-  const [quantitySelect, setQuantitySelect] = useState(1);
   const [cartItems, setCartItems] = useState([]);
 
   // se obtiene el contexto global
   const { showShoppingCart, setShowShoppingCart } = useContext(GlobalContext)
-  const {getCartData}=useShoppingCart()
+  const { getCartData } = useShoppingCart()
+
+  // Se crea una estado que almacena una función
+  // Esto es para evitar que el efecto se ejecute en cada renderizado y resolverá el problema de la profundidad máxima de actualizaciones excedida. Además, mantendrá la funcionalidad deseada de obtener los datos del carrito cuando sea necesario.
+  const [getCartDataFunc] = useState(() => getCartData);
+
 
   useEffect(() => {
-  // Obtener los datos del carrito y actualizar el estado
-    const data = getCartData();
+    // Obtener los datos del carrito y actualizar el estado
+    const data = getCartDataFunc();
     setCartItems(data);
-  }, [showShoppingCart]);
+  }, [getCartDataFunc]);
 
-
+  function formatPrice(price) {
+    if (price === undefined || price === null) {
+      return '0';
+    }
+    return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+  }
+  // Calcula el total de la compra de productos
+  const calculateTotal = () => {
+    return cartItems.reduce((total, item) => {
+      let price = typeof item.price === 'string' ? parseFloat(item.price.replace('.', '')) : item.price;
+      return total + price * item.quantity;
+    }, 0);
+    
+  };
+  
 
   return <>
     <div id="sidebar" className={showShoppingCart ? "sidebar open" : "sidebar"}  >
@@ -26,9 +44,14 @@ const ShoppingCart = () => {
       <i id="close-sidebar" onClick={() => setShowShoppingCart(false)} className="close-icon" ><svg xmlns="http://www.w3.org/2000/svg" width="33" height="32" viewBox="0 0 33 32" fill="none">
         <path fillRule="evenodd" clipRule="evenodd" d="M16.5 32C25.3366 32 32.5 24.8366 32.5 16C32.5 7.16344 25.3366 0 16.5 0C7.66344 0 0.5 7.16344 0.5 16C0.5 24.8366 7.66344 32 16.5 32ZM24.7212 21.7598C25.322 22.3606 25.322 23.3347 24.7212 23.9355C24.1204 24.5363 23.1463 24.5363 22.5455 23.9355L16.5783 17.9683L10.5108 24.0358C9.90999 24.6366 8.93589 24.6366 8.33509 24.0358C7.73428 23.435 7.73428 22.4609 8.33509 21.86L14.4026 15.7926L8.62091 10.0109C8.0201 9.41012 8.0201 8.43601 8.62091 7.83521C9.22172 7.2344 10.1958 7.2344 10.7966 7.83521L16.5783 13.6169L22.2596 7.93548C22.8605 7.33468 23.8346 7.33468 24.4354 7.93548C25.0362 8.53629 25.0362 9.51039 24.4354 10.1112L18.754 15.7926L24.7212 21.7598Z" fill="#C4C4C4"></path>
       </svg></i>
-      {cartItems.map((product) => (
-                <CartItem id={product.id} imgUrl={product.image} name={product.name} price={product.price} quantity={product.quantity} />
-
+      {cartItems.map((product,index) => (
+        <CartItem 
+        key={`${product.id}-${index}-${Math.random().toString(36).substr(2, 9)}`}
+        id={product.id}
+        imgUrl={product.image}
+        name={product.name}
+        price={product.price}
+        quantity={product.quantity} />
       ))}
 
       <div className="cart-summary">
@@ -43,19 +66,21 @@ const ShoppingCart = () => {
             </tr>
           </thead>
           <tbody>
-            <tr className="cart-summary-item">
-              <td className="cart-summary-item-name">Nombre del producto</td>
-              <td className="cart-summary-item-unit-price">Precio unitario</td>
-              <td className="cart-summary-item-quantity">Cantidad</td>
-              <td className="cart-summary-item-total-price">Precio total</td>
-            </tr>
-            {/* <!-- Repite .cart-summary-item para cada producto en el carrito --> */}
+            {cartItems.map((product) => (
+              <tr className="cart-summary-item">
+                <td className="cart-summary-item-name">{product.name}</td>
+                <td className="cart-summary-item-unit-price">${formatPrice(product.price)}</td>
+                <td className="cart-summary-item-quantity">{product.quantity}</td>
+                <td className="cart-summary-item-total-price">{formatPrice(product.quantity * product.price)}</td>
+              </tr>
+            ))}
           </tbody>
         </table>
         <div className="cart-summary-total">
           <span>Total a pagar:</span>
-          <span className="cart-summary-total-price">$0.00</span>
+          <span className="cart-summary-total-price">${formatPrice(calculateTotal())}</span>
         </div>
+
         <div className="cart-summary-buttons">
           <button className="cart-summary-checkout">Comprar</button>
           <button className="cart-summary-remove-all">Remover</button>
