0cbd35ee35fb4698e4badc766dea14f32819adf7
Andres Briñez <brinezlopez08@gmail.com>
2024-07-15 14:08:41 -0500

    Se crea el hook relacionado con el carrito de compras

---
+++ src/hooks/useShoppingCart.js
@@ -0,0 +1,28 @@
+// hooks/useCart.js
+// Hook Personalizado
+import { useContext } from 'react';
+import { GlobalContext } from '../Context/GlobalContext';
+
+export function useShoppingCart() {
+  const { setShowShoppingCart, setQuantityIntemShoppingCart } = useContext(GlobalContext);
+
+  function saveItemCart(product) {
+    // Obtiene el carrito de compras y si no existe lo crea con un arreglo vacio 
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+    // Agrega el producto al carrito
+    cart.push(product);
+    // Almacena el carrito en el local storage
+    localStorage.setItem('cart', JSON.stringify(cart));
+
+    // Actualizar el estado global
+    setShowShoppingCart(true);
+    setQuantityIntemShoppingCart(cart.length);
+  }
+
+  function getCartData() {
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+    return cart;
+  }
+
+  return { saveItemCart, getCartData };
+}
