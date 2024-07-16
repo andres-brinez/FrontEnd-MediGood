46b1654a14ed6413eb396b982735e797e0e1e4ed
Andres Briñez <brinezlopez08@gmail.com>
2024-07-16 00:12:07 -0500

    Se agrega el compoente del nav

--- src/Layouts/Dashboard/index.js
+++ src/Layouts/Dashboard/index.js
@@ -1,11 +1,26 @@
 import HeaderDashboard from "../../Components/Header/DashBoard";
+import NavDashBoard from "../../Components/Nav/DashBoard";
 import "./style.css"
 
 function LayoutDashboard() {
-  return (
-    <HeaderDashboard/>
+  return <>
+    <HeaderDashboard />
+    {/* <!-- Contenido del Dashboard --> */}
+    <div class="dashboard-container">
 
-  )
+      {/* <!-- Barra de navegación lateral --> */}
+      <NavDashBoard/>
+
+      {/* <!-- Contenido principal --> */}
+    
+    </div>
+
+    {/* <footer>
+      <p>&copy; 2024 Medigood - Todos los derechos reservados</p>
+    </footer> */}
+
+
+  </>
 }
 
 export default LayoutDashboard;
