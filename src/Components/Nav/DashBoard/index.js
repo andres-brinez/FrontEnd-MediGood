5878195da543ad218d422a125d91a81ce97badca
Andres Briñez <brinezlopez08@gmail.com>
2024-07-16 11:28:36 -0500

    Se agrega el enlace al inventario

--- src/Components/Nav/DashBoard/index.js
+++ src/Components/Nav/DashBoard/index.js
@@ -1,18 +1,23 @@
+import { Link } from "react-router-dom";
 import "./style.css"
 function NavDashBoard() {
   return (
     <nav class="side-nav">
-        <ul>
-          <li><a href="#"><i class="fas fa-shopping-cart"></i> Pedidos</a></li>
-          <li><a href="#"><i class="fas fa-box"></i> Inventario</a></li>
+      <ul>
+        <li><a href="#"><i class="fas fa-shopping-cart"></i> Pedidos</a></li>
+        <li>
+          <Link to="/dashboard/inventory">
+            <i class="fas fa-box"></i> Inventario
+          </Link>
+        </li>
 
-          <li><a href="#"><i class="fas fa-shopping-bag"></i> Compras</a></li>
-          {/* <!-- <li><a href="#"><i class="fas fa-chart-bar"></i> Ventas</a></li> --> */}
-          <li><a href="#"><i class="fas fa-user"></i> Perfil</a></li>
-          <li><a href="#"><i class="fas fa-users"></i> Usuarios</a></li>
-          <li><a href="#"><i class="fas fa-sign-out-alt"></i> Salir</a></li>
-        </ul>
-      </nav>
+        <li><a href="#"><i class="fas fa-shopping-bag"></i> Compras</a></li>
+        {/* <!-- <li><a href="#"><i class="fas fa-chart-bar"></i> Ventas</a></li> --> */}
+        <li><a href="#"><i class="fas fa-user"></i> Perfil</a></li>
+        <li><a href="#"><i class="fas fa-users"></i> Usuarios</a></li>
+        <li><a href="#"><i class="fas fa-sign-out-alt"></i> Salir</a></li>
+      </ul>
+    </nav>
   );
 }
 
