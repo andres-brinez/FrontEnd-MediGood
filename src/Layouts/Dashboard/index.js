0795c2bf85462e66b25ce16298b592dd472a8d83
Andres Briñez <brinezlopez08@gmail.com>
2024-07-15 23:56:55 -0500

    Se crea el layout del dashboard con el componete del header

---
+++ src/Layouts/Dashboard/index.js
@@ -0,0 +1,11 @@
+import HeaderDashboard from "../../Components/Header/DashBoard";
+import "./style.css"
+
+function LayoutDashboard() {
+  return (
+    <HeaderDashboard/>
+
+  )
+}
+
+export default LayoutDashboard;
