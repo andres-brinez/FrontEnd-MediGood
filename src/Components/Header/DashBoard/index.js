0de97899e91b01a418a578403c4523066102b548
Andres Briñez <brinezlopez08@gmail.com>
2024-07-16 00:21:12 -0500

    Se le pone un Link a el h1 de medigood

--- src/Components/Header/DashBoard/index.js
+++ src/Components/Header/DashBoard/index.js
@@ -1,13 +1,16 @@
+import { Link } from "react-router-dom";
 import "./style.css"
 
 function HeaderDashboard() {
   return <header className="top-nav headerDashBoard">
+  <Link to={"/"}>
   <div className="left-section">
       <div className="dashboard-icon" onclick="toggleSideNav()">
           <img src="img/dashboard/hospital.png" alt=""dashboard Icon/>
       </div>
       <div className="brand">MediGood</div>
   </div>
+  </Link>
   <div className="user">Andres Briñez </div>
   <p  className="logout" >Cerrar sesión</p>
 </header>
