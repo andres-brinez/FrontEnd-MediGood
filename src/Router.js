3209f656e23d5d4598503ed95d18fccc1d8d779c
Andres Briñez <brinezlopez08@gmail.com>
2024-07-15 23:55:06 -0500

    Se crea la ruta del dashboard

--- src/Router.js
+++ src/Router.js
@@ -1,28 +1,31 @@
-import { BrowserRouter as Router, Switch, Route,Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Routes } from 'react-router-dom';
 import Register from "./Pages/Register/Register"
 import Login from "./Pages/Login";
 import Page404 from './Pages/404';
 import LayoutLanding from './Layouts/Landing';
 import ProductsDetails from './Components/ProductsDetails';
 import ProductsSearched from './Components/ProductsSearched';
+import LayoutDshboard from './Layouts/Dashboard';
 
 const RoutesHandler = () => {
   return (
     <Router>
       <Routes>
-      {/* Rutas anidadas */}
-        <Route path="/" exact element={<LayoutLanding/>} >
-        <Route path="/search/:searchTerm" element={<ProductsSearched />} />
-        <Route path="/product/:id" element={<ProductsDetails />} />
+        {/* Rutas anidadas */}
+        <Route path="/" exact element={<LayoutLanding />} >
+          <Route path="/search/:searchTerm" element={<ProductsSearched />} />
+          <Route path="/product/:id" element={<ProductsDetails />} />
+        </Route>
+        <Route path="/dashboard" exact element={<LayoutDshboard />} >
         </Route>
         <Route path="/register" exact element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="*" element={<Page404/>}/> {/* Si no exista la ruta  */}
+        <Route path="*" element={<Page404 />} /> {/* Si no exista la ruta  */}
         {/* <Route component={NotFound} /> */}
       </Routes>
     </Router>
   )
-  
+
 }
 
 export default RoutesHandler;
