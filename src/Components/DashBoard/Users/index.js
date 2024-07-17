e72d699636b85800a37daae5a27c5c389e2292fd
Andres Briñez <brinezlopez08@gmail.com>
2024-07-17 13:31:46 -0500

    Se crea el componente

---
+++ src/Components/DashBoard/Users/index.js
@@ -0,0 +1,76 @@
+function Users() {
+  return <>
+    <div class="container-header">
+      <h2 class="title">Usuarios</h2>
+      {/* <!-- Barra de búsqueda --> */}
+      <div class="search-bar">
+        <input type="text" placeholder="Buscar por ID " />
+        <button class="search-button">
+          <img class="search-button-img" src="../img/dashboard/lupa.png" alt="Imagen de lupa" />                    </button>
+      </div>
+    </div>
+
+    {/* <!-- Botones de Detalle, Editar y Ocultar --> */}
+    <div class="inventory-buttons">
+      <button class="add-button">Añadir</button>
+      <button class="detalle-button">Detalle</button>
+      <button class="edit-button">Editar</button>
+    </div>
+
+    {/* <!-- Tabla de Usuarios --> */}
+    <table class="table-users">
+      {/* <!-- Encabezados de la tabla --> */}
+      <thead>
+        <tr>
+          <th></th>
+          <th>ID</th>
+          <th>Nombre</th>
+          <th>Rol</th>
+          <th>Estado</th>
+        </tr>
+      </thead>
+      {/* <!-- Contenido de la tabla --> */}
+      <tbody>
+        {/* <!-- Filas con datos de usuarios --> */}
+        <tr>
+          <td><input type="checkbox" id="user001" /></td>
+          <td>001</td>
+          <td>Usuario 1</td>
+          <td>Administrador</td>
+          <td>Activo</td>
+        </tr>
+        <tr>
+          <td><input type="checkbox" id="user002" /></td>
+          <td>002</td>
+          <td>Usuario 2</td>
+          <td>Usuario</td>
+          <td>Inactivo</td>
+        </tr>
+        <tr>
+          <td><input type="checkbox" id="user003" /></td>
+          <td>003</td>
+          <td>Usuario 3</td>
+          <td>Usuario</td>
+          <td>Activo</td>
+        </tr>
+      </tbody>
+    </table>
+
+{/* TODO: Crear la páginación de la tabla */}
+    {/* <!-- Paginación de la tabla --> */}
+    <div class="pagination">
+      <div class="pagination-options">
+        <button>Anterior</button>
+        <span>1</span>
+        <span>2</span>
+        <span>3</span>
+        <span>4</span>
+        <span>...</span>
+        <button>Siguiente</button>
+      </div>
+      <button class="show-all-button">Mostrar Todos</button>
+    </div>  </>
+
+}
+
+export default Users;
