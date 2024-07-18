da417dbf8615cb52aae1f432e123e6755339e0e7
Andres Briñez <brinezlopez08@gmail.com>
2024-07-17 22:35:55 -0500

    Se agrega la función para mostrar el logout

--- src/Components/Header/DashBoard/index.js
+++ src/Components/Header/DashBoard/index.js
@@ -2,6 +2,12 @@ import { Link } from "react-router-dom";
 import "./style.css"
 
 function HeaderDashboard() {
+
+  function showLogoutConfirmation() {
+    var confirmationModal = document.getElementById("logoutConfirmation");
+    confirmationModal.style.display = "block";
+  }
+
   return <header className="top-nav headerDashBoard">
   <Link to={"/"}>
   <div className="left-section">
@@ -12,8 +18,10 @@ function HeaderDashboard() {
   </div>
   </Link>
   <div className="user">Andres Briñez </div>
-  <p  className="logout" >Cerrar sesión</p>
+  <p  className="logout" onClick={showLogoutConfirmation}>Cerrar sesión</p>
 </header>
+
+
 
 }
 
