e31518925825dc42741d68603c05e6dbb9172b06
Andres Briñez <brinezlopez08@gmail.com>
2024-07-17 22:35:15 -0500

    Se agrega el modal

--- src/Layouts/Dashboard/index.js
+++ src/Layouts/Dashboard/index.js
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import HeaderDashboard from "../../Components/Header/DashBoard";
 import NavDashBoard from "../../Components/Nav/DashBoard";
 import "./style.css"
@@ -6,19 +6,41 @@ import "./formDashboard.css"
 
 
 function LayoutDashboard() {
+
+  const navigation= useNavigate();
+
+  function logout() {
+    navigation("/")
+  }
+
+  function hideLogoutConfirmation() {
+    var confirmationModal = document.getElementById("logoutConfirmation");
+    confirmationModal.style.display = "none";
+}
+
+
   return <>
     <HeaderDashboard />
     {/* <!-- Contenido del Dashboard --> */}
     <div class="dashboard-container">
 
       {/* <!-- Barra de navegación lateral --> */}
-      <NavDashBoard/>
-
+      <NavDashBoard />
+      {/* <!-- Cuadro de confirmación de logout--> */}
+    
       {/* <!-- Contenido principal --> */}
       <main className="main-content main-content-dashboard">
       <Outlet />
       </main>
     
+    </div>
+    <div className="confirmation-modal" id="logoutConfirmation">
+        <div class="modal-content">
+            <span class="close" onClick={hideLogoutConfirmation}>&times;</span>
+            <p>¿Estás seguro de que quieres cerrar sesión?</p>
+            <button class="ok" onClick={logout}>Aceptar</button>
+            <button class="cancel" onClick={hideLogoutConfirmation}>Cancelar</button>
+        </div>
     </div>
 
     <footer className="footerDashBoard">
