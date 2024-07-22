bc5c7380ec05670dfb2bdd36fa1921ee25951820
Andres Briñez <brinezlopez08@gmail.com>
2024-07-22 14:40:34 -0500

    Se agrega la función addProduct con la conexión de la api

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,6 +1,21 @@
 import { urls } from "../resources/url"
 import httpService from "./general/http";
 
+
+export const addProduct = (formData) => {
+
+  const url = urls.addProduct;
+  const body = formData;
+  return httpService.postFormData(url, body)
+    .then((response) => {
+      if (response.status === 200 || response.status === 201) {
+        alert("Producto agregado con éxito");
+        return true;
+      } else {
+        alert("No se pudo agregar el producto");
+      }
+    })
+}
 export const getAllProducts=()=>{
   const url= urls.getAllProducts;
   return httpService.get(url).then((response) => response.json())
